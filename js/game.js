42932136052519c639676aa258e20cd98a326e16
William Bryk <williambryk@college.harvard.edu>
2019-04-17 17:16:19 -0400

    elliptical swaps

--- js/game.js
+++ js/game.js
@@ -1,5 +1,3 @@
-// import { trials, trialIds } from "./data";
-
 var canvas = document.getElementById("canvas");
 
 var ctx = canvas.getContext("2d");
@@ -15,13 +13,14 @@ CANVAS_HEIGHT = canvas.height;
 CENTER_X = CANVAS_WIDTH / 2;
 CENTER_Y = CANVAS_HEIGHT / 2;
 const numSwapPoints = 100;
+const ELLIPSE_RATIO = 0.4;
 
 const vertVal = Math.sqrt(3) / 2;
 const spacingRatio = 0.7;
 let circlePositions = [[0, (-vertVal * 2) / 3], [-0.5, vertVal / 3], [0.5, vertVal / 3]];
 circlePositions = circlePositions.map(x => [
   x[0] * CANVAS_HEIGHT * spacingRatio + CENTER_X,
-  x[1] * CANVAS_HEIGHT * spacingRatio + CENTER_Y
+  x[1] * CANVAS_HEIGHT * spacingRatio + CENTER_Y + (vertVal * spacingRatio * CANVAS_HEIGHT) / 6
 ]);
 
 var c1 = new Circle(circlePositions[0][0], circlePositions[0][1], CIRCLE_RADIUS, circleColors[0]);
@@ -45,7 +44,7 @@ function Circle(x, y, rad, color) {
     _this.hidden = false;
   })();
 
-  this.move = function(ctx, x, y) {
+  this.moveTo = function(ctx, x, y) {
     _this.x = x;
     _this.y = y;
   };
@@ -58,7 +57,7 @@ function Circle(x, y, rad, color) {
   };
   this.draw = function(ctx) {
     if (_this.hidden) {
-      sideWidth = _this.radius * 2.2;
+      sideWidth = _this.radius * 2.0;
       ctx.fillStyle = "grey";
       ctx.fillRect(_this.x - sideWidth / 2, _this.y - sideWidth / 2, sideWidth, sideWidth);
 
@@ -72,6 +71,12 @@ function Circle(x, y, rad, color) {
   };
 }
 
+function refreshCirclePositions() {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].moveTo(ctx, circlePositions[i][0], circlePositions[i][1]);
+  }
+}
+
 function drawAll() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   for (var i = 0; i < circles.length; i++) {
@@ -79,25 +84,33 @@ function drawAll() {
   }
 }
 
-function getSwapPath(c1X, c1Y, c2X, c2Y, clockwise) {
+function rotate(x, y, angle) {
+  const nx = x * Math.cos(angle) - y * Math.sin(angle);
+  const ny = y * Math.cos(angle) + x * Math.sin(angle);
+  return [nx, ny];
+}
+
+function getSwapPath(c1X, c1Y, c2X, c2Y) {
   let points;
-  if (clockwise) {
-    points = Array(numSwapPoints)
-      .fill(0)
-      .map((x, i) => {
-        const diffX = (c2X - c1X) / numSwapPoints;
-        const diffY = (c2Y - c1Y) / numSwapPoints;
-        return [c1X + (i + 1) * diffX, c1Y + (i + 1) * diffY];
-      });
-  } else {
-    points = Array(numSwapPoints)
-      .fill(0)
-      .map((x, i) => {
-        const diffX = (c1X - c2X) / numSwapPoints;
-        const diffY = (c1Y - c2Y) / numSwapPoints;
-        return [c2X + (i + 1) * diffX, c2Y + (i + 1) * diffY];
-      });
-  }
+  const diffX = c2X - c1X;
+  const diffY = c2Y - c1Y;
+  const d = Math.sqrt(diffX * diffX + diffY * diffY);
+  const a = d / 2;
+  const b = a * ELLIPSE_RATIO;
+  const ellipseAngle = Math.atan2(diffY, diffX);
+  const ellipseCenter = [c1X + diffX / 2, c1Y + diffY / 2];
+
+  points = Array(numSwapPoints)
+    .fill(0)
+    .map((x, i) => {
+      const t = Math.PI - (Math.PI * (i + 1)) / numSwapPoints;
+      const relX = a * Math.cos(t);
+      const relY = b * Math.sin(t);
+      const [absX, absY] = rotate(relX, relY, ellipseAngle);
+
+      return [ellipseCenter[0] + absX, ellipseCenter[1] + absY];
+    });
+
   return points;
 }
 
@@ -108,14 +121,14 @@ function runSwap(index1, index2) {
     const c2X = circles[index2].x;
     const c2Y = circles[index2].y;
 
-    const path1 = getSwapPath(c1X, c1Y, c2X, c2Y, true);
-    const path2 = getSwapPath(c1X, c1Y, c2X, c2Y, false);
+    const path1 = getSwapPath(c1X, c1Y, c2X, c2Y);
+    const path2 = getSwapPath(c2X, c2Y, c1X, c1Y);
 
     function doSwapAnimation(index1, index2, path1, path2, i) {
       if (i < numSwapPoints) {
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        circles[index1].move(ctx, path1[i][0], path1[i][1]);
-        circles[index2].move(ctx, path2[i][0], path2[i][1]);
+        circles[index1].moveTo(ctx, path1[i][0], path1[i][1]);
+        circles[index2].moveTo(ctx, path2[i][0], path2[i][1]);
 
         drawAll();
 
@@ -163,7 +176,7 @@ async function waitForNextKey(keyName) {
 
 function makeGuess() {}
 
-async function runTrial(swaps) {
+async function runTrial(swaps, trialIndex) {
   hideCircles();
   drawAll();
   await pause(1000);
@@ -171,23 +184,32 @@ async function runTrial(swaps) {
   for (let s = 0; s < swaps.length; s++) {
     const swap = swaps[s];
     await runSwap(swap[0] - 1, swap[1] - 1);
+    await pause(400);
   }
 
+  updateDiv("status", `Trial ${trialIndex + 1}: Make Guess`);
   makeGuess();
   await pause(2000);
   unhideCircles();
   drawAll();
 }
 
+function updateDiv(divId, text) {
+  document.getElementById(divId).innerText = text;
+}
+
 async function main() {
   requestAnimationFrame(drawAll);
 
   for (let i = 0; i < TRIALIDS.length; i++) {
+    updateDiv("status", `Trial ${i + 1}: Press 's' key to begin trial`);
     await waitForNextKey("s");
+    updateDiv("status", `Trial ${i + 1}`);
     trialId = TRIALIDS[i];
     const swaps = TRIALS[trialId].swaps;
-    await runTrial(swaps);
+    await runTrial(swaps, i);
   }
+  document.getElementById("status").innerText = "FINISHED!";
 }
 
 main();
